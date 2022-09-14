ff419217a86aba5de1bc0699d46ce68d37d85c5f
Your Name <you@example.com>
2022-09-14 13:21:10 +0300

    initial commit -m

---
+++ src/it/hillel/Main.java
@@ -0,0 +1,12 @@
+package it.hillel;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double longitude =  43.2104645;
+        System.out.println(longitude);
+
+        double latitude = 22.9520741;
+        System.out.println(latitude);
+    }
+}
